f0d40f4bc34c894e0b8ae7f3cf661768fafabda0
sumitkumar <sumitkumar1202@gmail.com>
2023-07-16 04:08:49 +0530

    commit 1.1...added sign in activity

--- app/src/main/java/com/example/ping_now/MainActivity.kt
+++ app/src/main/java/com/example/ping_now/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.ping_now
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.ping_now.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private var binding: ActivityMainBinding? = null
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -19,8 +22,16 @@ class MainActivity : AppCompatActivity() {
         fragmentArrList.add(StatusFragment())
         fragmentArrList.add(CallsFragment())
 
-        val VpAdapter = ViewPagerAdapter(this@MainActivity,supportFragmentManager, fragmentArrList)
-        binding!!.viewPgrId.adapter = VpAdapter
+
+        //user not logged in
+        auth = FirebaseAuth.getInstance()
+        if(auth.currentUser == null){
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        }
+
+        val vpAdapter = ViewPagerAdapter(this@MainActivity,supportFragmentManager, fragmentArrList)
+        binding!!.viewPgrId.adapter = vpAdapter
         binding!!.tabLayId.setupWithViewPager(binding!!.viewPgrId)
 
 
---
+++ app/src/main/java/com/example/ping_now/SignInActivity.kt
@@ -0,0 +1,43 @@
+package com.example.ping_now
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.ping_now.databinding.ActivitySigninBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySigninBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        //if user already signed in
+        if(auth.currentUser != null){
+            startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+            finish()
+        }
+
+        //else for user NOT signed in
+        binding.signInBtnId.setOnClickListener {
+            if(binding.enterPhoneEdtTxtId.text!!.isEmpty()){
+                Toast.makeText(this,"Please enter your phone number",Toast.LENGTH_LONG).show()
+            }else{
+                var intent = Intent(this, OTPActivity::class.java)
+                intent.putExtra("number", binding.enterPhoneEdtTxtId.text!!)
+                startActivity(intent)
+            }
+        }
+
+
+
+
+    }
+}
