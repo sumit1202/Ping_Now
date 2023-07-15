009aa180ec75682f378c35380fcc49b25be5fc45
sumitkumar <sumitkumar1202@gmail.com>
2023-07-16 00:06:58 +0530

    commit 1.0...

--- app/src/main/java/com/example/ping_now/MainActivity.kt
+++ app/src/main/java/com/example/ping_now/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.ping_now
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.ping_now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        val fragmentArrList = ArrayList<Fragment>()
+        fragmentArrList.add(ChatsFragment())
+        fragmentArrList.add(StatusFragment())
+        fragmentArrList.add(CallsFragment())
+
+        val VpAdapter = ViewPagerAdapter(this@MainActivity,supportFragmentManager, fragmentArrList)
+        binding!!.viewPgrId.adapter = VpAdapter
+        binding!!.tabLayId.setupWithViewPager(binding!!.viewPgrId)
+
+
+
     }
 }
